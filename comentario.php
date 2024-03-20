9134fd75f9849b2987eb654dd631e113aa43d323
Matheus Hladki <matheushladki5@gmail.com>
2024-03-20 09:12:58 -0300

    creacion de archivo comentario.php

---
+++ comentario.php
@@ -0,0 +1,15 @@
+<html> 
+    
+<body>
+<?php
+    //comentario 
+    echo "prueba";
+    /* Comentario
+    Multilinea */
+    echo "prueba";
+    echo "ultima prueba";
+    #comentario
+    ?>
+</body>
+
+</html>
