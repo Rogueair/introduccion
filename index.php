541c82df032a79f31124437abd2fd322897c3f47
Matheus Hladki <matheushladki5@gmail.com>
2024-03-20 13:06:51 -0300

    eliminacion multilinea

--- index.php
+++ index.php
@@ -6,9 +6,5 @@
 
 <body>
     <h1> titulo </h1>
-    <?php echo <<<END
-    En un mundo conectado,
-    donde cada segundo se envia millones de correos electronicos... 
-    END; ?>
 </body>
 </html>
