eed1913489a13ea2f70488d15e432a497030e619
Matheus Hladki <Matheushladki5@gmail.com>
2024-04-10 09:14:42 -0300

    print y var dump

---
+++ aaaaa.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $edad = 25;
+    $nombre = "mathius";
+    $estatura = 1.81;
+    var_dump($edad);
+    var_dump($nombre); 
+    //var_dump nos muesta el tipo de la variable, en el caso de string la cantidad de caracteres
+    var_dump($estatura);
+    print_r ($edad);
+    print_r ($nombre); 
+    //print_r hace nos muestra el valor de la variable
+    print_r ($estatura);
+    ?>
+</body>
+</html>
